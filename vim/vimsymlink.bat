65872c0494c94491410490f048b70c9421e0d1b1
Limegrass <multipresident@gmail.com>
2018-11-20 18:34:45 -0600

    Added UltiSnips folder symlink, changed sudo
    Changed sudo command and added UltiSnips symlink to the batch and shell
    scripts.

--- vim/vimsymlink.bat
+++ vim/vimsymlink.bat
@@ -1,3 +1,11 @@
-mklink %USERPROFILE%\_vimrc     %USERPROFILE%\git\configs\vim\_vimrc
-mklink %USERPROFILE%\_vsvimrc   %USERPROFILE%\git\configs\vim\_vsvimrc
-mklink %USERPROFILE%\_ideavimrc %USERPROFILE%\git\configs\vim\_ideavimrc
+IF NOT EXIST %USERPROFILE%\_vimrc ^
+    ( mklink %USERPROFILE%\_vimrc     %USERPROFILE%\git\configs\vim\_vimrc )
+IF NOT EXIST %USERPROFILE%\_vsvimrc ^
+    ( mklink %USERPROFILE%\_vsvimrc   %USERPROFILE%\git\configs\vim\_vsvimrc)
+IF NOT EXIST %USERPROFILE%\_ideavimrc ^
+    ( mklink %USERPROFILE%\_ideavimrc %USERPROFILE%\git\configs\vim\_ideavimrc )
+
+IF NOT EXIST %USERPROFILE%\vimfiles\ ^
+( mkdir %USERPROFILE%\vimfiles )
+
+mklink /D %USERPROFILE%\vimfiles\UltiSnips %USERPROFILE%\git\configs\vim\UltiSnips
--- vim/vimsymlink.sh
+++ vim/vimsymlink.sh
@@ -1,3 +1,9 @@
 ln -s ~/git/configs/vim/_vimrc     ~.vimrc
 ln -s ~/git/configs/vim/_ideavimrc ~.ideavimrc
 ln -s ~/git/configs/vim/_vsvimrc   ~.ideavimrc
+
+if [ ! -d "~/.vim"]; then
+    mkdir ~/.vim
+fi
+
+ln -s ~/git/configs/vim/UltiSnips  ~/.vim/UltiSnips
