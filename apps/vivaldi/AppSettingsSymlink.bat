d83fdb9103e50bfe08cc667ed22189f1d0ed4aa3
Limegrass <multipresident@gmail.com>
2019-05-04 16:10:53 -0500

    Vivaldi settings
    Vivaldi settings and symlinks

---
+++ apps/vivaldi/AppSettingsSymlink.bat
@@ -0,0 +1,2 @@
+IF NOT EXIST "%USERPROFILE%\AppData\Local\Vivaldi\User Data\Default\Local App Settings\mpognobbkildjkofajifpdfhcoklimli" ^
+    ( mklink "%USERPROFILE%\AppData\Local\Vivaldi\User Data\Default\Local App Settings\mpognobbkildjkofajifpdfhcoklimli" "%USERPROFILE%\git\configs\apps\vivaldi\Local App Settings\mpognobbkildjkofajifpdfhcoklimli" )
--- vim/vimsymlink.bat
+++ vim/vimsymlink.bat
@@ -6,9 +6,12 @@ IF NOT EXIST %USERPROFILE%\_ideavimrc ^
     ( mklink %USERPROFILE%\_ideavimrc %USERPROFILE%\git\configs\vim\_ideavimrc )
 
 IF NOT EXIST %USERPROFILE%\.vim\ ^
-( mkdir %USERPROFILE%\.vim )
+    ( mkdir  %USERPROFILE%\.vim )
 
-mklink /D %USERPROFILE%\.vim\UltiSnips %USERPROFILE%\git\configs\vim\UltiSnips
+IF NOT EXIST %USERPROFILE%\.vim\UltiSnips ^
+    ( mklink /D %USERPROFILE%\.vim\UltiSnips %USERPROFILE%\git\configs\vim\UltiSnips )
 
-mklink %USERPROFILE%\.vim\coc-settings.json %USERPROFILE%\git\configs\vim\coc-settings.json
-mklink %USERPROFILE%\AppData\Local\nvim\coc-settings.json %USERPROFILE%\git\configs\vim\coc-settings.json
+IF NOT EXIST %USERPROFILE%\.vim\coc-settings.json ^
+    ( mklink %USERPROFILE%\.vim\coc-settings.json %USERPROFILE%\git\configs\vim\coc-settings.json )
+IF NOT EXIST %USERPROFILE%\AppData\Local\nvim\coc-settings.json ^
+    ( mklink %USERPROFILE%\AppData\Local\nvim\coc-settings.json %USERPROFILE%\git\configs\vim\coc-settings.json )
