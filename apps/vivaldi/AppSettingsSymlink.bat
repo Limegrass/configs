73156dc366d9ae83843e8b50db3b95d22aede341
James Ni <James.Ni@corp.sekoww.com>
2019-05-07 14:07:53 -0500

    Vivaldi batch modification
    Copying instead of symlinking.

--- apps/vivaldi/AppSettingsSymlink.bat
+++
@@ -1,2 +0,0 @@
-IF NOT EXIST "%USERPROFILE%\AppData\Local\Vivaldi\User Data\Default\Local App Settings\mpognobbkildjkofajifpdfhcoklimli" ^
-    ( mklink "%USERPROFILE%\AppData\Local\Vivaldi\User Data\Default\Local App Settings\mpognobbkildjkofajifpdfhcoklimli" "%USERPROFILE%\git\configs\apps\vivaldi\Local App Settings\mpognobbkildjkofajifpdfhcoklimli" )
