1e0ef011ee8af39a8d1d9ff6f641d38c9989706e
Limegrass <multipresident@gmail.com>
2019-05-08 20:06:37 -0500

    Fixes
    Proper Linux Compatibility and typo on symlink.

--- vim/vimsymlink.sh
+++ vim/vimsymlink.sh
@@ -1,6 +1,6 @@
-ln -s ~/git/configs/vim/_vimrc     ~.vimrc
-ln -s ~/git/configs/vim/_ideavimrc ~.ideavimrc
-ln -s ~/git/configs/vim/_vsvimrc   ~.ideavimrc
+ln -s ~/git/configs/vim/_vimrc     ~/.vimrc
+ln -s ~/git/configs/vim/_ideavimrc ~/.ideavimrc
+ln -s ~/git/configs/vim/_vsvimrc   ~/.ideavimrc
 
 if [ ! -d "~/.vim"] then
     mkdir ~/.vim
@@ -12,4 +12,5 @@ ln -s ~/git/configs/vim/coc-settings.json  ~/.vim/coc-settings.json
 if [ ! -d "~/.configs/nvim"] then
     mkdir -p ~/.configs/nvim
 fi
+
 echo "source ~/.vimrc" >> ~/.configs/nvim/init.vim
