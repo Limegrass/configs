bc401037408b3e1aab7e490250828c98dc62458d
Limegrass <jani17@alumni.davidson.edu>
2019-09-30 18:31:51 -0500

    Initial .bashrc and .tmux.conf

--- dotfiles/dotfiles.symlink.sh
+++ dotfiles/dotfiles.symlink.sh
@@ -1,3 +1,7 @@
 # TODO: Confirmation to remove and replace
+# TODO: Make sure the script is executed in
+# git root and then use git rev-parse --show-toplevel
 rm ~/.inputrc
 ln -S ~/.inputrc ~/git/configs/dotfiles/.inputrc
+ln -S ~/.bashrc ~/git/configs/dotfiles/.bashrc
+ln -S ~/.tmux.conf ~/git/configs/dotfiles/dotfiles.symlink.sh
