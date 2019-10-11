767c6694c32018a1b50f47f0595b345478f6096a
Limegrass <jani17@alumni.davidson.edu>
2019-10-11 00:50:02 -0500

    Added Xmodmap for CTRL on caps

--- dotfiles/dotfiles.symlink.sh
+++ dotfiles/dotfiles.symlink.sh
@@ -1,7 +1,8 @@
 # TODO: Confirmation to remove and replace
 # TODO: Make sure the script is executed in
 # git root and then use git rev-parse --show-toplevel
-rm ~/.inputrc
-ln -S ~/.inputrc ~/git/configs/dotfiles/.inputrc
-ln -S ~/.bashrc ~/git/configs/dotfiles/.bashrc
-ln -S ~/.tmux.conf ~/git/configs/dotfiles/dotfiles.symlink.sh
+# rm ~/.inputrc
+ln -s ~/.inputrc ~/git/configs/dotfiles/.inputrc
+ln -s ~/.bashrc ~/git/configs/dotfiles/.bashrc
+ln -s ~/.tmux.conf ~/git/configs/dotfiles/.tmux.conf
+ln -s ~/.Xmodmap ~/git/configs/dotfiles/.Xmodmap
